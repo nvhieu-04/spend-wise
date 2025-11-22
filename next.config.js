00c6c24ec214eb1cc475acd1da3eca7f4d686d1b
Nguyễn Văn Hiếu <ngvnhieu4@gmail.com>
2025-11-22 17:55:20 +0700

    feat: add fast-average-color dependency for improved color processing

--- next.config.js
+++ next.config.js
@@ -5,6 +5,15 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.logo.dev",
+      },
+    ],
+  },
+};
 
 export default config;
